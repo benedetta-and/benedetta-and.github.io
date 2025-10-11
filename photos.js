6fb188442c2b0c064d1f55f5e79dda26511aa689
benedetta-and <benedetta.andreasi@gmail.com>
2025-10-11 13:11:20 +0200

    js button weirdness

--- photos.js
+++ photos.js
@@ -1,11 +1,15 @@
 function scrollLeft(btn) {
-  console.log('Left button clicked'); 
-  const filmstrip = btn.parentElement.querySelector('.images');
-  filmstrip.scrollBy({ left: -300, behavior: 'smooth' });
+  const images = btn.nextElementSibling;
+  images.scrollBy({
+  left: -300,
+  behavior: 'smooth'
+  });
 }
 
 function scrollRight(btn) {
-  console.log('Right button clicked');
-  const filmstrip = btn.parentElement.querySelector('.images');
-  filmstrip.scrollBy({ left: 300, behavior: 'smooth' });
+  const images = btn.previousElementSibling;
+  images.scrollBy({
+  left: 300,
+  behavior: 'smooth'
+  });
 }
